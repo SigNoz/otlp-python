4f951f28b765c9106dc221f88e1a08cf13d768aa
Prashant Shahi <prashant@signoz.io>
2024-09-11 02:51:04 +0530

    feat: include healthz endpoint
    Signed-off-by: Prashant Shahi <prashant@signoz.io>

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 import random
-import string
 import time
 import json
 import os
@@ -40,14 +39,11 @@ set_logger_provider(logger_provider)
 exporter = OTLPLogExporter(endpoint=os.getenv("OTLP_ENDPOINT", "localhost:4317"), insecure=json.loads(os.getenv("INSECURE", "true").lower()))
 exporterspan = OTLPSpanExporter(endpoint=os.getenv("OTLP_ENDPOINT", "localhost:4317"), insecure=json.loads(os.getenv("INSECURE", "true").lower()))
 
-
 # add the batch processors to the trace provider
 logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
 trace.get_tracer_provider().add_span_processor(
     BatchSpanProcessor(exporterspan)
 )
-
-
 
 handler = LoggingHandler(level=logging.DEBUG,logger_provider=logger_provider)
 # Attach OTLP handler to root logger
@@ -60,18 +56,17 @@ logger2 = logging.getLogger("myapp.area2")
 logger2.addHandler(handler)
 logger2.setLevel(logging.DEBUG)
 
-
 tracer = trace.get_tracer(__name__)
-
-
 app = Flask(__name__)
 
 app.logger.addHandler(handler)
 
-
 def get_random_time():
     return random.randint(100, 1500) / 1000.0
 
+@app.route("/healthz")
+def health_check():
+	return jsonify({"status": "ok"})
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
@@ -87,10 +82,8 @@ def home():
                     time.sleep(get_random_time())
                 with tracer.start_as_current_span("authenticate_check_db"):
                     time.sleep(get_random_time())
-
                 with tracer.start_as_current_span("check_request limit"):
                     time.sleep(get_random_time())
-            
             with tracer.start_as_current_span("get_cart"):
                 logger2.debug(metadata + '"message": "getting cart for user: '+ user +'"}')
                 with tracer.start_as_current_span("check cart in cache"):
@@ -98,11 +91,9 @@ def home():
                 with tracer.start_as_current_span("check cart in db"):
                     logger2.debug(metadata + '"message": "checking cart from db for : '+ user +'"}')
                     time.sleep(get_random_time())
-            
             logger2.info(metadata + '"method":"GET", "status": 200, "level": "info", "message": "completed request for user: '+ user +'"}', extra={"method": "GET", "status": 200, "level": "info"})
             return "Hello " + user
-  
-  
+
 # driver function
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
